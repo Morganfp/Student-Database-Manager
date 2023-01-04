bb6e5f01d50f4d3ca7923d8aefb6cc52aed3cf22
Morgan Purcell <109186494+Morganfp@users.noreply.github.com>
2023-01-04 18:50:20 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 /*
 Author: Morgan Purcell
-Date: November 22cnd 2022
-Purpose: A program that uses a MySQL/C++ connector to build tables and run queries in MySQL, allowing students to easily store and track their course, grade, semester, and transcript data.
+Date: November 22nd 2022
+Purpose: A program written in C++ that uses a MySQL connector to create tables and run queries in a MySQL database. 
+          It stores student data such as courses, grades, semesters, and transcript information.
 */
 
 // Include the libraries needed
@@ -48,46 +49,58 @@ int main()
     std::cout << "Done!" << std::endl;
 
     // Connect to the MySQL database
-    std::cout << "Connecting to remote DB ..."; 
-    std::cout.flush();
-    sql::Connection* conn = driver->connect(db_host, db_user, db_password);
-    conn->setSchema(db_name);
-
-    // Call the createTables function to create the neccessary tables within the database
-    createTables(conn);
-
-    // Prompt the user to select a function or quit the program using 'q'
-    std::string input;
-    while(input != "q")
+    try
     {
-        // Prompt for user input
-        std::cout << ">>> ";
-        std::cin >> input;
+        std::cout << "Connecting to remote DB ..."; 
+        std::cout.flush();
+        sql::Connection* conn = driver->connect(db_host, db_user, db_password);
+        conn->setSchema(db_name);
 
-        // If/else blocks to call the desired function
-        if(input == "a")
+        // Call the createTables function to create the neccessary tables within the database
+        createTables(conn);
+
+        // Display a welcome screen using ANSI escape sequences for colored text
+        std::cout << "\n\n\n\033[100mWelcome to the Student Data Management program! Here is a list of available commands:\033[0m\n\n" << "\033[92ma (add)\033[0m\n c: course\n g: grade\n m: semester\n s: student\n t: completed course\n\n" << "\033[92ml (list)\033[0m\n c: courses\n g: grades\n m: semesters\n s: students\n t: completed courses\n\n" << "\033[92mt (student transcript)\033[0m\n\n"  << "\033[93md (delete student)\033[0m\n\n" << "\033[91mq (quit the program)\033[0m\n\n";
+
+        // Prompt the user to select a function or quit the program using 'q'
+        std::string input;
+        while(input != "q")
         {
-            addFunc(conn);
+            // Prompt for user input
+            std::cout << ">>> ";
+            std::cin >> input;
+
+            // If/else blocks to call the desired function
+            if(input == "a")
+            {
+                addFunc(conn);
+            }
+
+            else if(input ==  "l")
+            {
+                listFunc(conn);
+            }
+
+            else if(input == "t")
+            {
+                transcriptFunc(conn);
+            }
+
+            else if(input == "d")
+            {
+                deleteFunc(conn);
+            }
         }
 
-        else if(input ==  "l")
-        {
-            listFunc(conn);
-        }
-
-        else if(input == "t")
-        {
-            transcriptFunc(conn);
-        }
-
-        else if(input == "d")
-        {
-            deleteFunc(conn);
-        }
+        // Close the MySQL connection at the end of the program
+        conn->close();
     }
 
-    // Close the MySQL connection at the end of the program
-    conn->close();
+    // Catch and display any errors
+    catch(sql::SQLException sqle)
+    {
+        std::cout << "Exception in SQL: " << sqle.what() << std::endl;
+    }
 
     return 0;
 }
